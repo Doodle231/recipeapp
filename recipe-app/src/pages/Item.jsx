d08d024a0b5ec3ac9f74d24963fcfdc70a6be338
Doodle23 <Doodle23@yahoo.com>
2022-07-12 15:17:03 +0900

    fixed some mobile styling

--- recipe-app/src/pages/Item.jsx
+++ recipe-app/src/pages/Item.jsx
@@ -40,15 +40,18 @@ export function Item() {
       <div className="mt-8    text-2xl font-bold text-left mx-32"> Food Category</div>
       <img src = {foodItem.image} className =" md:h-[400px] md:w-[400px] md:mx-32 mt-8 md:object-fill"></img>
       </div>
-      <div className="overlay bg-gray-400 rounded-full h-[100px] w-[30%]  md:mx-32 absolute bottom-10 ">
+      <div className="overlay bg-gray-400 md:rounded-full h-[100px] md:w-[30%] w-full md:mx-32 absolute bottom-10 ">
       <h1 className='text-center pt-4 text-lg text-black font-extrabold'>Food Name</h1>
       <div className="div text-center text-md italic">description of food here</div>
       </div>
       
-<div className="rightcontainer bg-yellow-200 w-[30%] md:h-3/4 md:absolute right-32 top-24 relative text-center">
-<div className ="bg-gray-300 w-80 h-[30%]  m-auto mt-16">
+<div className="rightcontainer bg-yellow-200 md:w-[30%] md:h-3/4 md:absolute md:right-32 m-auto md:top-24 pt-4 relative text-center"> Ingredients
+<div className ="bg-gray-300 w-80 h-[30%]  m-auto mt-4">1
 </div>
-<div className ="bg-gray-300 w-80 h-[30%]  m-auto mb-16 mt-16">
+
+<div className="pt-4">Recipe</div>
+
+<div className ="bg-gray-300 w-80 h-[30%]  m-auto mb-16 mt-4">2
 </div>
 </div>
 
--- recipe-app/src/pages/home.jsx
+++ recipe-app/src/pages/home.jsx
@@ -15,27 +15,28 @@ function Home() {
     
     <div className ="topsection grid grid-cols-2  h-full bg-emerald-500  ">
         
-        <div className = "hungry-section pl-2 sm:pl-12 pt-4 w-full text-white">
-        <h1 className = "text-white text-lg pt-6 font-extrabold md:text-5xl">Feeling Hungry? </h1>
-        <div className ="text-white pt-3 h-56 w-32  md:w-3/4 text-sm md:text-xl">You can use this app to search for a meal that pleases you. </div>
+        <div className = "hungry-section pl-2 sm:pl-12 pt-4 w-full text-white pb-12">
+        <h1 className = "text-white text-lg pt-6 font-extrabold md:text-5xl pl-8">Feeling Hungry? </h1>
+        <div className ="text-white pt-3 h-56 w-32  md:w-3/4 text-sm md:text-xl pl-8">You can use this app to search for a meal that pleases you. </div>
         
         
-         <h1 className =" text-sm w-full md:text-2xl">Search for a recipe</h1>
-         <input placeholder = " Type here to search " className = "recipesearch  bg-gray-300 w-auto mt-2 md:h-8 text-black" ></input>
-         <button className ="bg-green-700 w-20  md:ml-8 rounded-md ml-6 mt-2">Search </button>
+         <h1 className =" text-sm w-full md:text-2xl pl-8">Search for a recipe</h1>
+         <input placeholder = " Type here to search " className = "recipesearch  bg-gray-300 w-auto mt-2 md:h-8 text-black ml-8" ></input>
+         <button className ="bg-green-700 w-20  md:ml-8 rounded-md ml-8 mt-2">Search </button>
         
         </div>
 
         <div className='w-full h-full  '>
-        <img src = {herofood} className = "w-full h-[60%] sm:w-full sm:h-full sm:pr-14  pt-12 object-fit align-bottom pr-2" alt =""></img>
+        <img src = {herofood} className = "w-screen h-3/4 sm:w-full sm:h-full sm:pr-14  sm:pt-12 object-fit align-bottom pr-8 right-0 pl-1 pt-8" alt =""></img>
         </div>
+
     </div>
 
      <div className="midsection  w-full   sm:bg-foodbg sm:backdrop-brightness-50 sm:pl-2 ">
      
      <div className=" text-zinc-200 text-4xl  text-center pt-4 font-bold">Categories</div>
 
-     <div className="cardcontainer w-full h-96 sm:flex flex-row font-bold text-2xl mt-12 gap-x-5 ">
+     <div className="cardcontainer w-full h-96 sm:flex flex-row font-bold text-2xl mt-8 gap-x-5 mb-2 ">
      
      <Card name ="Appetizer" location ="/Appetizer"/>
      <Card name ="Main Course" location ="MainCourse"/>
@@ -45,9 +46,7 @@ function Home() {
      </div>
      </div>
 
-<div className='w-full h-[500px] bg-yellow-500 '>
-<div>hey</div>
-</div>
+
 
     </div>
   )
